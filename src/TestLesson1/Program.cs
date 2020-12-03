ca56b8a84ad8459919c90cdb3d4c59b443da66fa
Tatsiana Malinina <63168842+molliny42@users.noreply.github.com>
2020-12-03 23:40:36 +0300

    init: created pfoject TestLesson1

---
+++ src/TestLesson1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
